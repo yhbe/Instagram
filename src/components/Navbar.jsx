e5dd97e57d502901274a73d582449435346813ad
SHEF <Shefqetmush@gmail.com>
2023-02-05 21:31:04 -0500

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,52 @@
 import "./Navbar.css"
+import React from "react";
 
 function Navbar() {
+  const [showButtons,setShowButtons] = React.useState(false)
+
+  const removeButtons = () => {
+    if (window.innerWidth <= 960){
+      setShowButtons(false)
+    } else {
+      setShowButtons(true)
+    }
+  }
+
+  window.addEventListener("resize", () => removeButtons())
+
+  React.useEffect(() => {
+    removeButtons()
+  }, [])
+
+  function createButtons(){
+    return (
+      <ul className="buttons-ul">
+        <li><i class="fa-sharp fa-solid fa-house-chimney"></i></li>
+        <li> <i class="fa-regular fa-comment"></i></li>
+        <li><i class="fa-regular fa-heart"></i></li>
+        <li><i class="fa-regular fa-user"></i></li>
+      </ul>
+    );
+  }
+
   return (
     <div className="navbar--container">
       <div className="logo-container">
         <div className="logo--group">
-        <img className="logo--image" src="./instagramimage.png"></img>
-        <h1>Instagram</h1>
+          <img className="logo--image" src="./instagramimage.png"></img>
+          <h1>Instagram</h1>
         </div>
       </div>
       <div className="searchbox--container">
         <input className="search--input" type="text" placeholder="Search" />
       </div>
-      <div className="buttons-container">
-        <i class="fa-sharp fa-solid fa-house-chimney"></i>
-        <i class="fa-regular fa-comment"></i>
-        <i class="fa-regular fa-heart"></i>
-        <i class="fa-regular fa-user"></i>
-      </div>
+      {showButtons ? (
+        <div className="buttons-container">
+          {showButtons && createButtons()}
+        </div>
+      ) : (
+        <div className="mobile--container">{createButtons()}</div>
+      )}
     </div>
   );
 }
